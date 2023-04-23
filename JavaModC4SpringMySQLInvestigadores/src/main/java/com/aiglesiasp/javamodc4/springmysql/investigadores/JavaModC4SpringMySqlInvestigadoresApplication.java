77138701b99b780b5fe0a9fb5c530d6ceb38c12b
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-23 03:28:52 +0200

    Init Commit

---
+++ JavaModC4SpringMySQLInvestigadores/src/main/java/com/aiglesiasp/javamodc4/springmysql/investigadores/JavaModC4SpringMySqlInvestigadoresApplication.java
@@ -0,0 +1,13 @@
+package com.aiglesiasp.javamodc4.springmysql.investigadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaModC4SpringMySqlInvestigadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaModC4SpringMySqlInvestigadoresApplication.class, args);
+	}
+
+}
---
+++ JavaModC4SpringMySQLInvestigadores/src/test/java/com/aiglesiasp/javamodc4/springmysql/investigadores/JavaModC4SpringMySqlInvestigadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiglesiasp.javamodc4.springmysql.investigadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaModC4SpringMySqlInvestigadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
